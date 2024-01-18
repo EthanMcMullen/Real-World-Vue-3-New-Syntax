5b9a49297e7e08831f32b2ed4cd851093c49662e
EthanMcMullen <113043142+EthanMcMullen@users.noreply.github.com>
2024-01-18 10:45:50 -0500

    a

---
+++ src/views/EventList.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import EventCard from '@/components/EventCard.vue'
+
+import { ref } from 'vue'
+ 
+
+const events = ref([]);
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
